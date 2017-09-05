aeccae6e6bdc3b940978533dda71d599791528f1
Sergey Safonov <spoof@spoofa.info>
2017-09-05 22:00:29 +0300

    Added Singlestat panel and tests

--- dashboard.go
+++ dashboard.go
@@ -204,7 +204,8 @@ type Panel interface {
 type panelType string
 
 const (
-	textPanel panelType = "text"
+	textPanel       panelType = "text"
+	singlestatPanel panelType = "singlestat"
 )
 
 type dummyPanel struct {
@@ -223,6 +224,8 @@ func (p *dummyPanel) UnmarshalJSON(data []byte) error {
 	switch jp.Type {
 	case textPanel:
 		panel = new(TextPanel)
+	case singlestatPanel:
+		panel = new(SinglestatPanel)
 	default:
 		return nil
 	}
@@ -299,6 +302,97 @@ func (p *TextPanel) MarshalJSON() ([]byte, error) {
 
 func (p *TextPanel) UnmarshalJSON(data []byte) error {
 	type JSONPanel TextPanel
+	jp := struct {
+		*JSONPanel
+		*PanelGeneralOptions
+		Type *panelType `json:"type"`
+	}{
+		JSONPanel:           (*JSONPanel)(p),
+		PanelGeneralOptions: p.GeneralOptions(),
+		Type:                &p.GeneralOptions().panelType,
+	}
+
+	if err := json.Unmarshal(data, &jp); err != nil {
+		return err
+	}
+
+	return nil
+}
+
+// SinglestatPanel  represents Singlestat panel.
+type SinglestatPanel struct {
+	PanelGeneralOptions
+
+	// Options. Value.
+	ValueName       string `json:"valueName"`     // Stat: min/max/avg/current/total/name/first/delta/diff/range
+	ValueFontSize   string `json:"valueFontSize"` // 0%-100% TODO: validation
+	Postfix         string `json:"postfix"`
+	PostfixFontSize string `json:"postfixFontSize"` // 0%-100% TODO: validation
+	Prefix          string `json:"prefix"`
+	PrefixFontSize  string `json:"prefixFontSize"` // 0%-100% TODO: validation
+	Format          string `json:"format"`         // Unit option. TODO: make a custom type with constants
+
+	// Options. Coloring.
+	// Colorize background or not
+	ColorBackground bool `json:"colorBackground"`
+	// Colorize value or not
+	ColorValue bool     `json:"colorValue"`
+	Thresholds string   `json:"thresholds"` // comma separated values "x,x". TODO: validation
+	Colors     []string `json:"colors"`     // array of 3 colors, ie. rgba(50, 172, 45, 0.97)
+
+	// Options. Spark lines.
+	SparkLine struct {
+		Show       bool   `json:"show"`
+		FullHeight bool   `json:"full"`
+		LineColor  string `json:"lineColor"` // ie. rgba(50, 172, 45, 0.97)
+		FillColor  string `json:"fillColor"` // ie. rgba(50, 172, 45, 0.97)
+	} `json:"sparkline"`
+
+	// Options. Gauge
+	Gauge struct {
+		Show             bool `json:"show"`
+		MaxValue         int  `json:"maxValue"`
+		MinValue         int  `json:"minValue"`
+		ThresholdLabels  bool `json:"thresholdLabels"`
+		ThresholdMarkers bool `json:"thresholdMarkers"`
+	} `json:"gauge"`
+
+	generalOptions PanelGeneralOptions
+}
+
+// NewSinglestatPanel creates new "Singlestat" panel.
+func NewSinglestatPanel() *SinglestatPanel {
+	return &SinglestatPanel{
+		generalOptions: PanelGeneralOptions{
+			panelType: singlestatPanel,
+			MinSpan:   12,
+			Links:     []*PanelLink{}, // to make [] in marshaling instead of nil
+		},
+	}
+}
+
+func (p *SinglestatPanel) GeneralOptions() *PanelGeneralOptions {
+	return &p.generalOptions
+}
+
+func (p *SinglestatPanel) MarshalJSON() ([]byte, error) {
+	type JSONPanel SinglestatPanel
+	jp := struct {
+		*JSONPanel
+		*PanelGeneralOptions
+		ID   uint      `json:"id"`
+		Type panelType `json:"type"`
+	}{
+		JSONPanel:           (*JSONPanel)(p),
+		PanelGeneralOptions: p.GeneralOptions(),
+		ID:                  p.GeneralOptions().id,
+		Type:                p.GeneralOptions().panelType,
+	}
+	return json.Marshal(jp)
+}
+
+func (p *SinglestatPanel) UnmarshalJSON(data []byte) error {
+	type JSONPanel SinglestatPanel
 	jp := struct {
 		*JSONPanel
 		*PanelGeneralOptions
---
+++ panel_singlestat_test.go
@@ -0,0 +1,173 @@
+package grafana
+
+import (
+	"encoding/json"
+	"reflect"
+	"testing"
+)
+
+func TestSinglestatPanel_MarshalJSON(t *testing.T) {
+	panel := NewSinglestatPanel()
+	panel.ValueName = "max"
+	panel.ValueFontSize = "50%"
+	panel.Prefix = "prefix"
+	panel.PrefixFontSize = "75%"
+	panel.Postfix = "postfix"
+	panel.PostfixFontSize = "25%"
+	panel.Format = "currencyJPY"
+	panel.ColorBackground = true
+	panel.ColorValue = true
+	panel.Thresholds = "20,80"
+	panel.Colors = []string{"rgba(10, 172, 45, 0.97)", "rgba(20, 172, 45, 0.97)", "rgba(30, 172, 45, 0.97)"}
+
+	panel.SparkLine.Show = true
+	panel.SparkLine.FullHeight = true
+	panel.SparkLine.LineColor = "rgb(31, 120, 193)"
+	panel.SparkLine.FillColor = "rgba(50, 61, 71, 0.18)"
+
+	panel.Gauge.Show = true
+	panel.Gauge.MaxValue = 100
+	panel.Gauge.MinValue = 50
+	panel.Gauge.ThresholdLabels = true
+	panel.Gauge.ThresholdMarkers = true
+
+	options := panel.GeneralOptions()
+	options.Title = "New Panel"
+	options.Description = "Panel Description"
+	options.Height = "250px"
+	options.MinSpan = 1
+	options.Span = 12
+	options.Transparent = true
+
+	got, err := json.MarshalIndent(panel, "", "\t\t")
+	if err != nil {
+		t.Fatalf("TextPanel.MarshalJSON returned error %s", err)
+	}
+	expected := []byte(`{
+		"valueName": "max",
+		"valueFontSize": "50%",
+		"prefix": "prefix",
+		"prefixFontSize": "75%",
+		"postfix": "postfix",
+		"postfixFontSize": "25%",
+		"format": "currencyJPY",
+		"colorBackground": true,
+		"colorValue": true,
+		"thresholds": "20,80",
+		"colors": [
+			"rgba(10, 172, 45, 0.97)",
+			"rgba(20, 172, 45, 0.97)",
+			"rgba(30, 172, 45, 0.97)"
+		],
+		"sparkline": {
+			"show": true,
+			"full": true,
+			"lineColor": "rgb(31, 120, 193)",
+			"fillColor": "rgba(50, 61, 71, 0.18)"
+		},
+		"gauge": {
+			"show": true,
+			"maxValue": 100,
+			"minValue": 50,
+			"thresholdLabels": true,
+			"thresholdMarkers": true
+		},
+		"description": "Panel Description",
+		"height": "250px",
+		"links": [],
+		"minSpan": 1,
+		"span": 12,
+		"title": "New Panel",
+		"transparent": true,
+		"id": 0,
+		"type": "singlestat"
+	}`)
+	if eq, err := JSONBytesEqual(expected, got); err != nil {
+		t.Fatalf("SinglestatPanel.MarshalJSON returned error %s", err)
+	} else if !eq {
+		t.Errorf("SinglestatPanel.MarshalJSON:\nexpected: %s\ngot: %s", expected, got)
+	}
+}
+
+func TestSinglestatPanel_UnmarshalJSON(t *testing.T) {
+	expected := NewSinglestatPanel()
+	expected.ValueName = "max"
+	expected.ValueFontSize = "50%"
+	expected.Prefix = "prefix"
+	expected.PrefixFontSize = "75%"
+	expected.Postfix = "postfix"
+	expected.PostfixFontSize = "25%"
+	expected.Format = "currencyJPY"
+	expected.ColorBackground = true
+	expected.ColorValue = true
+	expected.Thresholds = "20,80"
+	expected.Colors = []string{"rgba(10, 172, 45, 0.97)", "rgba(20, 172, 45, 0.97)", "rgba(30, 172, 45, 0.97)"}
+
+	expected.SparkLine.Show = true
+	expected.SparkLine.FullHeight = true
+	expected.SparkLine.LineColor = "rgb(31, 120, 193)"
+	expected.SparkLine.FillColor = "rgba(50, 61, 71, 0.18)"
+
+	expected.Gauge.Show = true
+	expected.Gauge.MaxValue = 100
+	expected.Gauge.MinValue = 50
+	expected.Gauge.ThresholdLabels = true
+	expected.Gauge.ThresholdMarkers = true
+
+	options := expected.GeneralOptions()
+	options.Title = "New Panel"
+	options.Description = "Panel Description"
+	options.Height = "250px"
+	options.MinSpan = 1
+	options.Span = 12
+	options.Transparent = true
+
+	data := []byte(`{
+		"valueName": "max",
+		"valueFontSize": "50%",
+		"postfix": "postfix",
+		"postfixFontSize": "25%",
+		"prefix": "prefix",
+		"prefixFontSize": "75%",
+		"format": "currencyJPY",
+		"colorBackground": true,
+		"colorValue": true,
+		"thresholds": "20,80",
+		"colors": [
+		"rgba(10, 172, 45, 0.97)",
+		"rgba(20, 172, 45, 0.97)",
+		"rgba(30, 172, 45, 0.97)"
+		],
+		"sparkline": {
+			"show": true,
+			"full": true,
+			"lineColor": "rgb(31, 120, 193)",
+			"fillColor": "rgba(50, 61, 71, 0.18)"
+		},
+		"gauge": {
+			"show": true,
+			"maxValue": 100,
+			"minValue": 50,
+			"thresholdLabels": true,
+			"thresholdMarkers": true
+		},
+		"description": "Panel Description",
+		"height": "250px",
+		"links": [],
+		"minSpan": 1,
+		"span": 12,
+		"title": "New Panel",
+		"transparent": true,
+		"id": 0,
+		"type": "singlestat"
+	}`)
+	var got SinglestatPanel
+	err := json.Unmarshal(data, &got)
+	if err != nil {
+		t.Fatalf("SinglestatPanel.UnmarshalJSON returned error %s", err)
+	}
+
+	if !reflect.DeepEqual(expected, &got) {
+		t.Errorf("SinglestatPanel.MarshalJSON:\nexpected: %#v\ngot: %#v", expected, &got)
+	}
+}
