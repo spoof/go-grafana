6173beb1294fba572d92af85aac8722ece65d0b6
Sergey Safonov <spoof@spoofa.info>
2017-09-06 01:27:00 +0300

    Move Panel and PanelLink to a separate file

---
+++ grafana/panel.go
@@ -0,0 +1,100 @@
+// Copyright 2017 Sergey Safonov
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+// http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package grafana
+
+import "encoding/json"
+
+type Panel interface {
+	GeneralOptions() *PanelGeneralOptions
+}
+
+type panelType string
+
+const (
+	textPanel       panelType = "text"
+	singlestatPanel panelType = "singlestat"
+)
+
+type dummyPanel struct {
+	Type panelType `json:"type"`
+	Panel
+}
+
+func (p *dummyPanel) UnmarshalJSON(data []byte) error {
+	type JSONPanel dummyPanel
+	var jp JSONPanel
+	if err := json.Unmarshal(data, &jp); err != nil {
+		return err
+	}
+
+	var panel Panel
+	switch jp.Type {
+	case textPanel:
+		panel = new(TextPanel)
+	case singlestatPanel:
+		panel = new(SinglestatPanel)
+	default:
+		return nil
+	}
+
+	if err := json.Unmarshal(data, panel); err != nil {
+		return err
+	}
+
+	p.Panel = panel
+	return nil
+}
+
+type PanelGeneralOptions struct {
+	id        uint
+	panelType panelType
+
+	Description string       `json:"description"`
+	Height      string       `json:"height"`
+	Links       []*PanelLink `json:"links"`
+	MinSpan     uint         `json:"minSpan"` // TODO: valid values: 1-12
+	Span        uint         `json:"span"`    // TODO: valid values: 1-12
+	Title       string       `json:"title"`
+	Transparent bool         `json:"transparent"`
+}
+
+type panelLinkType string
+
+const (
+	PanelLinkAbsolute  panelLinkType = "absolute"
+	PanelLinkDashboard panelLinkType = "dashboard"
+)
+
+type PanelLink struct {
+	IncludeVars  bool          `json:"includeVars"`
+	KeepTime     bool          `json:"keepTime"`
+	Params       string        `json:"params"`
+	OpenInNewTab bool          `json:"targetBlank"`
+	Type         panelLinkType `json:"type"`
+
+	// type=absolute
+	Title string `json:"title,omitempty"`
+	URL   string `json:"url,omitempty"`
+
+	// type=dashboard
+	DashboardURI string `json:"dashUri,omitempty"`   // TODO: validation. should be valid dashboard
+	Dashboard    string `json:"dashboard,omitempty"` // actually it's title. Autofilled from dashboard title
+}
+
+// NewPanelLink creates new PanelLink
+func NewPanelLink(linkType panelLinkType) *PanelLink {
+	return &PanelLink{
+		Type: linkType,
+	}
+}
